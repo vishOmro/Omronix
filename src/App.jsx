3ed1052b53fbfce954f070695433a609ff645c34
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2025-02-22 15:49:08 +0530

    Basic React Template

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="min-w-screen min-h-screen bg-black text-white text-2xl flex justify-center align-center items-center">
+      Hello EveryOne
+    </div>
+    </>
+  )
+}
+
+export default App
