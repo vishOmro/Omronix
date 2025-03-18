f8728d1770df61c45cc51a4947c5430a1d4abd3e
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2025-03-18 19:13:14 +0530

    Error Fixed4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import AboutUsSection from "../components/HomePageComponent/AboutUsSection.jsx"
 import HeroSection from "../components/HomePageComponent/heroSection.jsx"
-import KeyValueProposition from "../components/HomePageComponent/KeyValueProposition.jsx"
+import Key from "../components/HomePageComponent/KeyValueProposition.jsx"
 import Product from "../components/HomePageComponent/Product.jsx"
 import ProductSnapShot from "../components/HomePageComponent/ProductSnapShot.jsx"
 // import Card from "../components/Card"
@@ -13,7 +13,7 @@ const Home = () => {
     <div className="w-full min-h-screen  text-black bg-background-blue">
     <HeroSection />
     <AboutUsSection />
-    <KeyValueProposition />
+    <Key />
     <ProductSnapShot />
     <Product />
 
