802b5370256418d511690f2029f64c1f1b8cb5d6
Shreyapatil2142 <shreyapatil2142@gmail.com>
2025-03-18 12:13:49 +0530

    Productdetails

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,7 @@
 
 import Hero from "../components/ProductPageComponent/Hero.jsx"
 import WhyChooseUs from "../components/ProductPageComponent/WhyOmronix.jsx"
-import ProductsDetail from "../components/ProductPageComponent/productsDetail.jsx"
+import ProductsDetail from "../components/ProductPageComponent/ProductsDetail.jsx"
 import ProductSolutions from "../components/ProductPageComponent/productSolutions.jsx"
 
 
